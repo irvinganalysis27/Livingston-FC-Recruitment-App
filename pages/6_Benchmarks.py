da8616f9778c3ceab0eebe22a0f80e5b0bca6f0b
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 10:45:13 +0100

    Update 6_Benchmarks.py

--- pages/6_Benchmarks.py
+++ pages/6_Benchmarks.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from auth import check_password
 
-check_password()
+if not check_password():
+    st.stop()
 
 st.title("Benchmarks Page")
 st.write("This page will show benchmarks such as average, 75th percentile, and 90th percentile for each position.")
