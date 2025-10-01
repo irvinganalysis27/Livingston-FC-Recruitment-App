d78a6df591883cd8142bdd396cc4a16771f7d325
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 09:49:24 +0100

    Update 1_Player_Search_and_Radar.py

--- pages/1_Player_Search_and_Radar.py
+++ pages/1_Player_Search_and_Radar.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from auth import check_password
+
+check_password()
 
 st.title("Player Search & Radar Page")
 st.write("This page will let you search for a player and generate radar plots based on their position’s metric group.")
