46f979aefea5f5c023a69683600f5b62f78f23ff
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 10:33:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from auth import check_password
 
-# Run auth once at the very top
+st.set_page_config(page_title="Livingston FC Recruitment App", layout="wide")
+
+# Run auth check at the very start
 if not check_password():
     st.stop()
 
-# Redirect user to the welcome page
+# If login OK, send user straight to Home
 st.switch_page("pages/0_Home.py")
