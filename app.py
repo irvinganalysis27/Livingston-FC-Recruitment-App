2e7eabed5dc9f5e7db993cc4ec7f9f34e9eb1424
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 11:16:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-# streamlit_app.py
 import streamlit as st
 from auth import check_password
 
-st.set_page_config(page_title="Livingston FC Recruitment App", layout="wide")
-
+# Run password check globally
 if not check_password():
     st.stop()
 
-# Hide the "app" item in the sidebar
-hide_menu_css = """
-    <style>
-        section[data-testid="stSidebar"] ul li:first-child {display: none;}
-    </style>
-"""
-st.markdown(hide_menu_css, unsafe_allow_html=True)
-
-# Optional: redirect automatically to Home page
-# st.switch_page("pages/0_Home.py")
-
+# This file just acts as a landing page
 st.title("Welcome Page")
 st.markdown("✅ You are logged in. Use the sidebar to navigate.")
