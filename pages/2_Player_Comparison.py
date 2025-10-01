762a4f9fa40a20a3d63a37d55d23684329364dcb
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 10:44:34 +0100

    Update 2_Player_Comparison.py

--- pages/2_Player_Comparison.py
+++ pages/2_Player_Comparison.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from auth import check_password
 
-check_password()
+if not check_password():
+    st.stop()
 
 st.title("Player Comparison Page")
 st.write("This page will allow selecting 2–3 players and show side-by-side radars and comparison tables.")
