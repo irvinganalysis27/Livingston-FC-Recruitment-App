2e4d598dae42fa09b90ad0758849ee66ccc1ea8f
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 10:45:04 +0100

    Update 5_Weightings.py

--- pages/5_Weightings.py
+++ pages/5_Weightings.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from auth import check_password
 
-check_password()
+if not check_password():
+    st.stop()
 
 st.title("Weightings Page")
 st.write("This page will have sliders to adjust metric weightings by position and recalculate rankings live.")
