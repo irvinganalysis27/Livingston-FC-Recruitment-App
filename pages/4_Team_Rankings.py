1810f4fdd6b6f1aeea0f4476f674945ab2584ab4
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 09:49:54 +0100

    Update 4_Team_Rankings.py

--- pages/4_Team_Rankings.py
+++ pages/4_Team_Rankings.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from auth import check_password
+
+check_password()
 
 st.title("Team Rankings Page")
 st.write("This page will show a team’s best XI by ranking and allow comparison of league-only vs whole dataset.")
