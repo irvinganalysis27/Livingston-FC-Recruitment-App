ca43de2ccd92f9647c75a9231d790e7ec8f5df67
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 09:49:44 +0100

    Update 3_Best_Players_by_Position.py

--- pages/3_Best_Players_by_Position.py
+++ pages/3_Best_Players_by_Position.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from auth import check_password
+
+check_password()
 
 st.title("Best Players by Position Page")
 st.write("This page will show leaderboards of the best players per position with filters for leagues and minutes played.")
