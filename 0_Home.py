c609f6b5ea84e69bac29bb63a2128b2472f990d7
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 11:31:55 +0100

    Update 0_Home.py

--- 0_Home.py
+++ 0_Home.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from auth import check_password
+from branding import show_branding
 
 st.set_page_config(page_title="Livingston FC Recruitment App", layout="wide")
 
-# Protect this page too
+# Protect page
+from auth import check_password
 if not check_password():
     st.stop()
+
+# Show branding header
+show_branding()
 
 st.title("Welcome Page")
 st.markdown("✅ You are logged in. Use the sidebar to navigate.")
