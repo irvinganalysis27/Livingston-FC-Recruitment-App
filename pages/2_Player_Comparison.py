327fb9e325c2d7c8012ca7b039332a2d5616264e
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 13:51:49 +0100

    Update 2_Player_Comparison.py

--- pages/2_Player_Comparison.py
+++ pages/2_Player_Comparison.py
@@ -773,6 +773,6 @@ fig = radar_compare(
     labels_clean, A_vals, B_vals,
     A_name=pA, B_name=pB,
     labels_to_genre=labels_to_genre,
-    genre_colors=GENRE_BG,
+    genre_colors=group_colors   # <-- FIXED
 )
 st.pyplot(fig, use_container_width=True)
