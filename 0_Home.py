5952fcf3151a65230560668260742dce02b03b61
irvinganalysis27 <irvinganalysis@gmail.com>
2025-10-01 10:34:09 +0100

    Update 0_Home.py

--- 0_Home.py
+++ 0_Home.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from auth import check_password
 
-# Set config first
 st.set_page_config(page_title="Livingston FC Recruitment App", layout="wide")
 
-# Run password check first
-check_password()
+# Protect this page too
+if not check_password():
+    st.stop()
 
-# If logged in, just show a welcome message (acts like a landing page)
 st.title("Welcome Page")
 st.markdown("✅ You are logged in. Use the sidebar to navigate.")
