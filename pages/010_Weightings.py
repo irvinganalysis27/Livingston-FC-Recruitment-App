bf90f72d3d2c96a533e2186f53fc144e11c65e92
irvinganalysis27 <irvinganalysis@gmail.com>
2025-12-16 10:25:26 +0000

    Update 010_Weightings.py

--- pages/010_Weightings.py
+++ pages/010_Weightings.py
@@ -2,21 +2,15 @@ import streamlit as st
 import pandas as pd
 from auth import check_password
 from branding import show_branding
+from ui.sidebar import render_sidebar
 
 st.set_page_config(page_title="Livingston FC Recruitment App", layout="centered")
-
-st.markdown(
-    """
-    <style>
-        [data-testid="stSidebarNav"] { display: none; }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 # ---------- Authentication ----------
 if not check_password():
     st.stop()
+
+render_sidebar()
 
 # ---------- Branding ----------
 show_branding()
