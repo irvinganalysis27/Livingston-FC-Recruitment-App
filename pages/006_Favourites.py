844617d332788d6e37b02b2e516eb38db736f337
irvinganalysis27 <irvinganalysis@gmail.com>
2026-01-22 09:08:25 +0000

    password fix on favourites page

--- pages/006_Favourites.py
+++ pages/006_Favourites.py
@@ -12,6 +12,8 @@ from ui.sidebar import render_sidebar
 import sys
 import os
 
+from auth import check_password
+
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
